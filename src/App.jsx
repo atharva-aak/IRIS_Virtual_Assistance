8523d9c946756cc98aababc14516bffdb603d196
atharva-aak <atharvakalbande5@gmail.com>
2025-01-28 02:32:11 +0530

    Created the User Interface

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import "./App.css"
+import va from "./assets/ai.png"
+import { MdMic } from "react-icons/md";
+import { datacontext } from './context/UserContext';
+
+function App() {
+ let {recognition} =  useContext(datacontext)
+  return (
+    <div className='main'>
+
+      <img src={va} alt="" id ="vira"/>
+      <span>I'm Vera, Your Virtual Assistant</span>
+      <button onClick={()=>{
+        recognition.start()
+      }}> Click Here <MdMic /> </button>
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext }  from 'react'
+export const datacontext=createContext()
+
+function UserContext({children}) {
+
+    function speak(text){
+        let text_speak = new SpeechSynthesisUtterance(text)
+        text_speak.volume=1;
+        text_speak.rate=1;
+        text_speak.pitch=1;
+        text_speak.lang="hi-GB"
+        window.speechSynthesis.speak(text_speak)
+
+
+    }
+let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+let recognition = new speechRecognition()
+recognition.onresult=(e)=>{
+    console.log(e)
+}
+
+   let value={
+recognition
+   }
+  return (
+    <div>
+        <datacontext.Provider value={value}>
+      {children}
+      </datacontext.Provider>
+    </div>
+  )
+}
+
+export default UserContext
